dfdf5c867491fcb206e96854f3918c11a82968de
Jose Rodriguez <jerodriguez2804@gmail.com>
2026-06-09 20:09:47 -0700

    adding firebase

--- app.js
+++ app.js
@@ -27,13 +27,13 @@ import {
   Replace this placeholder with your own Firebase config later.
 */
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_AUTH_DOMAIN_HERE",
-  projectId: "YOUR_PROJECT_ID_HERE",
-  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "YOUR_APP_ID_HERE",
-  measurementId: "YOUR_MEASUREMENT_ID_HERE"
+  apiKey: "AIzaSyAVsbugDlLYIiN7D70OrzZ94wdzOV2CVgk",
+  authDomain: "fir-time-card.firebaseapp.com",
+  projectId: "fir-time-card",
+  storageBucket: "fir-time-card.firebasestorage.app",
+  messagingSenderId: "329685379883",
+  appId: "1:329685379883:web:249ce9cbef76b49b4f736e",
+  measurementId: "G-GKV8F9SBBN"
 };
 
 const adminEmails = [
